f910722588d5c041e5855066d0827135ed383663
ThiagoHRCosta <thiagohrcosta86@gmail.com>
2023-05-09 22:26:19 -0300

    Fix typo

--- app/main.py
+++ app/main.py
@@ -8,9 +8,10 @@ shift = int(input("Type the shift number: \n"))
 def encrypt(plain_text, shift_amount):
   cipher_text = ""
   for letter in plain_text:
-    position = alphabet.index(letter)
+    position = alphabet.letters.index(letter)
     new_position = position + shift_amount
-    new_letter = alphabet[new_position]
+    new_letter = alphabet.letters[new_position]
     cipher_text += new_letter
   print(f"The encoded text is {cipher_text}")
-    
+
+encrypt(plain_text=text, shift_amount=shift)
